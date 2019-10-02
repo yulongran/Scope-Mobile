429c02eb87b231fd29a3f7a257e2eaa6edafb1ff
Yulong Ran <yulongran@gmail.com>
2019-10-01 23:34:52 -0700

    Added project page

---
+++ Scope/Components/Project.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react'
+import {
+    Text,
+    View,
+    StyleSheet,
+    Dimensions,
+    Button,
+    TouchableOpacity
+} from 'react-native'
+import {stringify} from 'qs'
+
+export class Project extends Component
+{
+    /**
+     * Construct a ProjectDisplay object
+     * @param {*} props
+     * @property projectName the name of the project
+     * @property courseName the name of the course
+     * @property schoolName the name of the institution
+     * @property startDate the start date of the project
+     * @property endDate the end date of the project
+     */
+
+    constructor(props)
+    {
+        super(props)
+    }
+    render()
+    {
+        return (
+            <TouchableOpacity>
+                <View style={styles.ViewStyle}>
+                    <Text style={styles.ProjectNameStyle}>{this.props.projectName}</Text>
+                    <Text style={styles.CourseStyle}>{this.props.courseName} {this.props.schoolName}</Text>
+                    <Text style={styles.startDateStyle}>Start {this.props.startDate}</Text>
+                    <Text style={styles.endDateStyle}>End {this.props.endDate}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+const WIDTH = Dimensions
+    .get('window')
+    .width
+const styles = StyleSheet.create({
+    ViewStyle: {
+        width: WIDTH*0.38,
+        height: 100,
+        borderColor: 'blue',
+        borderWidth: 1,
+        borderRadius: 10,
+        margin: 10
+    },
+    ProjectNameStyle: {
+        paddingTop: 7,
+        color: 'blue',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    CourseStyle: {
+        textAlign: 'center',
+        paddingTop: 10
+    },
+    startDateStyle: {
+        paddingTop: 10,
+        textAlign: 'center',
+        fontSize: 12
+    },
+    endDateStyle: {
+        textAlign: 'center',
+        fontSize: 12
+    }
+})
+
+export default Project
--- Scope/Screen/ProjectScreen.js
+++ Scope/Screen/ProjectScreen.js
@@ -1,59 +1,173 @@
-import React, {Component} from 'react'
-import {View, Text, StyleSheet, FlatList} from 'react-native'
-import MenuButton from '../Components/MenuButton'
-import ProjectDisplay from '../Components/ProjectDisplay'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  Dimensions
+} from "react-native";
+import MenuButton from "../Components/MenuButton";
+import Project from "../Components/Project";
 
-class ProjectScreen extends Component 
-{
-    state = {
-        Projects: [
-            {
-                projectName : 'Scope',
-                courseName: 'CS122 SJSU',
-                date: '08/01/2019 - 12/01/2019',
-            }, 
-            {
-                projectName : 'HelloWorld',
-                courseName: 'CS66 SJSU',
-                date: '08/01/2019 - 12/01/2019',
-            },
-        ]
+class ProjectScreen extends Component {
+  state = {
+    Projects: [
+      {
+        projectName: "Scope0",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2017-12-01"
+      },
+      {
+        projectName: "Scope",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2019-12-01"
+      },
+      {
+        projectName: "Scope1",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2019-12-01"
+      },
+      {
+        projectName: "Scope2",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2019-12-01"
+      },
+      {
+        projectName: "Scope3",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2018-12-01"
+      },
+      {
+        projectName: "Scope4",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2018-12-01"
+      },
+      {
+        projectName: "Scope5",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2018-12-01"
+      },
+      {
+        projectName: "Scope6",
+        courseName: "CS122",
+        schoolName: "SJSU",
+        startDate: "2019-08-01",
+        endDate: "2018-12-01"
+      }
+    ]
+  };
 
-    }
-    render()
-    {
-        return(
-            <View style = {styles.container}>
-                <MenuButton navigation = {this.props.navigation}/>
-                <FlatList style = {styles.listStyle}>
-                {
-                    this.state.Projects.map((project)=>
-                    {
-                        return(
-                            <ProjectDisplay projectName={project.projectName} courseName= {project.courseName}
-                            date= {project.date}/>
-                        )
-                    }
-                    )
-                }
-                </FlatList>
-            </View>
-        )
-    }
+  getHistoryProject() {
+    return (history = this.state.Projects.filter(function(project) {
+      return Date.parse(project.endDate) < Date.now();
+    }));
+  }
+
+  getOngoingProject() {
+    return (ongoing = this.state.Projects.filter(function(project) {
+      return Date.parse(project.endDate) >= Date.now();
+    }));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MenuButton navigation={this.props.navigation} />
+        <View style={styles.sectionStlye}>
+          <Text style={styles.textStyle}>Ongoing</Text>
+        </View>
+        <SafeAreaView
+          style={{ height: Dimensions.get("window").height * 0.35 }}
+        >
+          <FlatList
+            data={this.getOngoingProject()}
+            renderItem={({ item }) => (
+              <Project
+                projectName={item.projectName}
+                courseName={item.courseName}
+                schoolName={item.schoolName}
+                startDate={item.startDate}
+                endDate={item.endDate}
+              />
+            )}
+            numColumns={2}
+          />
+        </SafeAreaView>
+        <View style={styles.historySectionStlye}>
+          <Text style={styles.textStyle}>History</Text>
+        </View>
+        <SafeAreaView
+          style={{ height: Dimensions.get("window").height * 0.35 }}
+        >
+            <FlatList
+            data={this.getHistoryProject()}
+            renderItem={({ item }) => (
+              <Project
+                projectName={item.projectName}
+                courseName={item.courseName}
+                schoolName={item.schoolName}
+                startDate={item.startDate}
+                endDate={item.endDate}
+              />
+            )}
+            numColumns={2}
+          />
+        </SafeAreaView>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create(
-    {
-        container: {
-            flex:3,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent :'center',
-        },
-        text: {
-            fontSize: 30,
-        },
-    }
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: Dimensions.get("window").height * 0.12
+  },
+  listStyle: {
+    backgroundColor: "#fff"
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16
+  },
+  title: {
+    fontSize: 32
+  },
+  textStyle: {
+    textAlign: "left",
+    fontStyle: "italic"
+  },
+  sectionStlye: {
+    width: Dimensions.get("window").width * 0.8,
+    height: 20,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1
+  },
+  historySectionStlye: {
+    width: Dimensions.get("window").width * 0.8,
+    height: 20,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1
+  }
+});
 
 export default ProjectScreen;
